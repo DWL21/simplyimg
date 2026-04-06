a161290324429f5d2173c35a398676e82bb825f7
dwl21 <nggus5@gmail.com>
2026-04-06 22:56:05 +0900

    ci: commit compiled WASM pkg and remove CI build step

---
+++ packages/img-processor/pkg/img_processor_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const compress: (a: number, b: number, c: number, d: number, e: number) => [number, number, number, number];
+export const crop: (a: number, b: number, c: number, d: number, e: number, f: number) => [number, number, number, number];
+export const flip: (a: number, b: number, c: number) => [number, number, number, number];
+export const get_info: (a: number, b: number) => [number, number, number];
+export const resize: (a: number, b: number, c: number, d: number, e: number, f: number) => [number, number, number, number];
+export const rotate: (a: number, b: number, c: number) => [number, number, number, number];
+export const convert: (a: number, b: number, c: number, d: number, e: number) => [number, number, number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
